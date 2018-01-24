843216c5ec18b9b115ded3dd983f45d6bfe0a362
Shane Wells <shane130798@live.co.uk>
2018-01-24 14:02:29 +0000

    Added bare functionality to solution

--- Object Arrays/Object Arrays.cpp
+++ Object Arrays/Object Arrays.cpp
@@ -2,10 +2,45 @@
 //
 
 #include "stdafx.h"
+#include <array>
+#include <string>
+#include <iostream>
+#include <conio.h>
 
+using namespace std;
+
+class person {
+public:
+	void GetName(string sNamepass);
+	void DisplayName();
+
+protected:
+	string sName;
+
+};
+
+void person::GetName(string sNamepass)
+{
+	string sName = sNamepass;
+}
+
+void person::DisplayName()
+{
+	cout << sName;
+}
 
 int main()
 {
+	person oUsers[3];
+	string sName = "";
+	for (int iCount = 0; iCount < 3; iCount++) {
+
+		cout << "please enter the name for user " << iCount + 1;
+		cin >> sName;
+		oUsers[iCount].GetName(sName);
+
+
+	}
     return 0;
 }
 
